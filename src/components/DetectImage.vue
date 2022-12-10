6f6af33e0fefacd46915ff87fa717d914f25d899
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2022-12-09 19:09:10 -0500

    tidy up the bugs

--- src/components/DetectImage.vue
+++ src/components/DetectImage.vue
@@ -16,6 +16,8 @@
 </template>
 
 <script>
+    import * as cvstfjs from "customvision-tfjs";
+    import labels from "raw-loader!../../public/models/labels.txt";
     export default {
         name: 'DetectImage',
         props: {
@@ -25,7 +27,9 @@
             return {
                 image: 0,        // start on image 0
                 numImages: 10,   // 12 to start, had to delete two PNGs
-                predictions: [], // prevents reference undefined
+                labels: labels,
+                // model: null, // had to comment this out for the stackoverflow fix to work 
+                predictions: []
             }
         },
         computed: {
@@ -34,17 +38,43 @@
             },
             // no disable method needed where we're from :)
         },
+        async mounted() {
+            this.image = this.randomImage();
+
+            // load up a new model
+            this.model = new cvstfjs.ClassificationModel();
+            await this.model.loadModelAsync("models/model.json");
+
+            this.model = Object.freeze(this.model) // thanks stackoverflow! https://stackoverflow.com/questions/67815952/vue3-app-with-tensorflowjs-throws-typeerror-cannot-read-property-backend-of-u
+
+            // parse labels
+            this.labels = labels.split("\n").map(e => {
+                return e.trim();
+            });
+
+            // run prediction
+            this.predict();
+        },
         methods: {
             next() {
-                this.image = this.randomImage(this.image); // wrap around
+                this.image = this.randomImage(); // pick randomly
                 this.predictions = [];
                 setTimeout(this.predict, 500);
             },
-            randomImage(prev) { // pick random image instead
+            randomImage() { // pick random image instead
                 let choice = Math.floor( Math.random() * this.numImages ); // random choice
-                if( choice == prev ) choice = (choice + 1) % this.numImages; // oops
+                if( choice == this.image ) choice = (choice + 1) % this.numImages; // oops
                 return choice
-            }
+            },
+            async predict() {
+                //execute inference
+                let prediction = await this.model.executeAsync(this.$refs.img);
+                let label = prediction[0];
+                //build up a predictions object by parsing details to labels and probability
+                this.predictions = label.map((p, i) => {
+                    return { index: i, label: this.labels[i], probability: p * 100 };
+                });
+            },
         },
     }
 </script>
